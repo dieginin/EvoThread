aa34a76e8dadb77261cf21fa6c7cb1c3bd0fb20a
Diego Balestra <diegobalestra9@gmail.com>
2024-06-22 21:41:41 +0000

    feat: create client.py

---
+++ models/client.py
@@ -0,0 +1,28 @@
+class Client:
+    def __init__(self, id: int, name: str, address: str):
+        self.__id = id
+        self.__name = name
+        self.__address = address
+
+    @property
+    def id(self) -> int:
+        return self.__id
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def address(self) -> str:
+        return self.__address
+
+    def __repr__(self) -> str:
+        return self.name
+
+    def change_name(self, new_name: str):
+        self.__name = new_name
+        # TODO save to DB
+
+    def change_address(self, new_address: str):
+        self.__address = new_address
+        # TODO save to DB
